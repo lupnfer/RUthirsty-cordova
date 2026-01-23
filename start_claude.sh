9d6facd07257a2c7efca3faa02858e2bff32067a
charketh <hvbjpyrbk3580@hotmail.com>
2026-01-23 10:04:59 +0800

    Update start_claude.sh

--- start_claude.sh
+++ start_claude.sh
@@ -3,7 +3,7 @@ set -e
 #!/bin/bash
 
 # 设置环境变量
-export ANTHROPIC_AUTH_TOKEN=sk-zNTPXWGbXhjARCdCnEkoPQLqHXVQmo7v3yEHFJEKExi3Do3m
+export ANTHROPIC_AUTH_TOKEN=sk-W2dlfofLnJfkoe26haOiYQmzqFD0DQPBPmfZao8YOBuo0d92
 export ANTHROPIC_BASE_URL=https://apiHK.Ai
 # 备选API线路（去掉下面两行注释生效）
 # export ANTHROPIC_AUTH_TOKEN=sk-BnACKGNS5fruSd9Mmm2cH4MuLXGR27sB0md9KsYN6fvR4CYi
